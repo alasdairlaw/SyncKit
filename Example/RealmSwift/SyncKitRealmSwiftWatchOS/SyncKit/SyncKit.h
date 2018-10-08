8c09c0def01df41e21729f73dec5cb4aa86991ab
Alasdair Law <alasdair.law@icloud.com>
2018-10-08 18:34:56 +0100

    Add watchOS target

---
+++ Example/RealmSwift/SyncKitRealmSwiftWatchOS/SyncKit/SyncKit.h
@@ -0,0 +1,19 @@
+//
+//  SyncKit.h
+//  SyncKit
+//
+//  Created by Alasdair Law on 08/10/2018.
+//  Copyright © 2018 Manuel Entrena. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for SyncKit.
+FOUNDATION_EXPORT double SyncKitVersionNumber;
+
+//! Project version string for SyncKit.
+FOUNDATION_EXPORT const unsigned char SyncKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SyncKit/PublicHeader.h>
+
+
